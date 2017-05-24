b21112a296ae2f8707396fce86a9ab25873deff2
InAbsencia <absentia@protonmail.ch>
2017-05-24 20:35:24 +0200

    >(

--- 11_CDIO_FINAL/src/final_cdio_11/test/unit_suites/JDBCAllTests.java
+++ 11_CDIO_FINAL/src/final_cdio_11/test/unit_suites/JDBCAllTests.java
@@ -4,13 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import final_cdio_11.test.dao.TestListOperatorDAO;
-
 /*
  * JUnit Test Suite used to run all unit tests at once.
  */
 @RunWith(Suite.class)
-@SuiteClasses({ JDBCTableTests.class, JDBCViewTests.class, TestListOperatorDAO.class })
+@SuiteClasses({ JDBCTableTests.class, JDBCViewTests.class })
 public class JDBCAllTests {
 
 }
