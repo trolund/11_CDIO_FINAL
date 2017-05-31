f2ee1eef2ac80512fc7c590aa217d9d4e0c84d72
InAbsencia <absentia@protonmail.ch>
2017-05-31 20:05:07 +0200

    Small refactor.

--- 11_CDIO_FINAL/WebContent/assets/js/edit_users.js
+++ 11_CDIO_FINAL/WebContent/assets/js/edit_users.js
@@ -1,36 +1,35 @@
 // Edit user button
 $(document).ready(function() {
-    $(".edit_user").click(function() {
-    	var id = $(this).prop('name');
+	$(".edit_user").click(function() {
+		var id = $(this).prop('name');
         
-        $.getJSON('api/opr/RoleList/' + id, function(roleData) { 
+		$.getJSON('api/opr/RoleList/' + id, function(roleData) { 
+			$('#AdminRoleEdit').prop('checked', false);
+            $('#FarRoleEdit').prop('checked', false);
+            $('#VeakRoleEdit').prop('checked', false);
+            $('#LabRoleEdit').prop('checked', false);
             
-                $('#AdminRoleEdit').prop('checked', false);
-                $('#FarRoleEdit').prop('checked', false);
-                $('#VeakRoleEdit').prop('checked', false);
-                $('#LabRoleEdit').prop('checked', false);
+        	$.each(roleData, function(i, item) {	
+        		if (roleData[i].roleName == "Admin") {
+        			$('#AdminRoleEdit').prop('checked', true);
+        		}
             
-        	$.each(roleData, function(i, item) {
-        			
-            if(roleData[i].roleName == "Admin"){
-                $('#AdminRoleEdit').prop('checked', true);
-            }
-            if(roleData[i].roleName == "Farmaceut"){
-                $('#FarRoleEdit').prop('checked',true);
-            }
-            if(roleData[i].roleName == "Værkfører"){
-                $('#VeakRoleEdit').prop('checked',true);
-            }
-            if(roleData[i].roleName == "Laborant"){ 
-                $('#LabRoleEdit').prop('checked',true);
-            }
-                        
+        		if (roleData[i].roleName == "Farmaceut") {
+        			$('#FarRoleEdit').prop('checked', true);
+        		}
+            
+        		if(roleData[i].roleName == "Værkfører") {
+        			$('#VeakRoleEdit').prop('checked', true);
+        		}
+            
+        		if (roleData[i].roleName == "Laborant") { 
+        			$('#LabRoleEdit').prop('checked', true);
+        		}           
         	});
         });    
         
-        
         $.getJSON('api/opr/' + id, function(data) {
-        	console.log('User with id: ' + id + "loaded for edit");
+        	console.log('User with id: ' + id + " loaded for edit");
         	console.log(data);
             
         	$('#statusEdit').val(data.status);
@@ -41,9 +40,8 @@ $(document).ready(function() {
         	$('#oprEmailEdit').val(data.oprEmail);
         	$('#oprCprEdit').val(data.oprCpr);
             console.log('Checkbox input' + data.adminRole)
-        	
             
-        	console.log(data + "added to form")
+            console.log(data + "added to form")
         }); 
         
         $("#editUser_Box").show(400);
