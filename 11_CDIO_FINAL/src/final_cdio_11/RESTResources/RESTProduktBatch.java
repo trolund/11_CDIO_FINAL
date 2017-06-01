c0b8ab9d4f7b025125cbdb94961a7b21ad9c1de3
InAbsencia <absentia@protonmail.ch>
2017-06-01 09:23:28 +0200

    Change menu layout.

---
+++ 11_CDIO_FINAL/src/final_cdio_11/RESTResources/RESTProduktBatch.java
@@ -0,0 +1,8 @@
+package final_cdio_11.RESTResources;
+
+import javax.ws.rs.Path;
+
+@Path("/pb")
+public class RESTProduktBatch {
+
+}
---
+++ 11_CDIO_FINAL/src/final_cdio_11/RESTResources/RESTRaavareBatch.java
@@ -0,0 +1,8 @@
+package final_cdio_11.RESTResources;
+
+import javax.ws.rs.Path;
+
+@Path("/rb")
+public class RESTRaavareBatch {
+
+}
