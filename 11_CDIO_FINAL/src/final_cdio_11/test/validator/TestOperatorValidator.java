38cf8c2f1f07023f8e0c66e08493aa913a9a9595
tMascagni <t.mascagni@gmail.com>
2017-06-06 11:57:56 +0200

    TestOperatorValidator færdig.

--- 11_CDIO_FINAL/src/final_cdio_11/test/validator/TestOperatorValidator.java
+++ 11_CDIO_FINAL/src/final_cdio_11/test/validator/TestOperatorValidator.java
@@ -152,5 +152,287 @@ public class TestOperatorValidator {
 	/*
 	 * isOprIniValid tests.
 	 */
+	@Test
+	public void testIsOprIniValidPositive00() {
+		String oprIni = "22";
 
+		boolean actual = oprValidator.isOprIniValid(oprIni);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprIni + " is supposed to be valid.", expected, actual);
+	}
+	
+	public void testIsOprIniValidPositive01() {
+		String oprIni = "222";
+
+		boolean actual = oprValidator.isOprIniValid(oprIni);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprIni + " is supposed to be valid.", expected, actual);
+	}
+	
+	public void testIsOprIniValidPositive02() {
+		String oprIni = "2222";
+
+		boolean actual = oprValidator.isOprIniValid(oprIni);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprIni + " is supposed to be valid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprIniValidNegative00() {
+		String oprIni = "1";
+
+		boolean actual = oprValidator.isOprIniValid(oprIni);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprIni + " is supposed to be invalid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprIniValidNegative01() {
+		String oprIni = "1111111111";
+
+		boolean actual = oprValidator.isOprIniValid(oprIni);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprIni + " is supposed to be invalid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprIniValidNegative02() {
+		String oprIni = "1111111111111111111111111";
+
+		boolean actual = oprValidator.isOprIniValid(oprIni);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprIni + " is supposed to be invalid.", expected, actual);
+	}
+	
+	
+	/*
+	 * isOprEmailValid test.
+	 */
+	@Test
+	public void testIsOprEmailValidPositive00() {
+		String oprEmail = "t.mascagni@gmail.com";
+
+		boolean actual = oprValidator.isOprEmailValid(oprEmail);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprEmail + " is supposed to be valid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprEmailValidPositive01() {
+		String oprEmail = "iyyelsec@gmail.com";
+
+		boolean actual = oprValidator.isOprEmailValid(oprEmail);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprEmail + " is supposed to be valid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprEmailValidPositive02() {
+		String oprEmail = "b.farazan@gmail.com";
+
+		boolean actual = oprValidator.isOprEmailValid(oprEmail);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprEmail + " is supposed to be valid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprEmailValidNegative00() {
+		String oprEmail = "1111111111111111111111111@fail.aes";
+
+		boolean actual = oprValidator.isOprIniValid(oprEmail);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprEmail + " is supposed to be invalid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprEmailValidNegative01() {
+		String oprEmail = "iRplebl0rd@haxor.1337";
+
+		boolean actual = oprValidator.isOprIniValid(oprEmail);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprEmail + " is supposed to be invalid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprEmailValidNegative02() {
+		String oprEmail = "Zygsej0102@irzyg.0l123";
+
+		boolean actual = oprValidator.isOprIniValid(oprEmail);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprEmail + " is supposed to be invalid.", expected, actual);
+	}
+	
+	/*
+	 * isOprCprValid test.
+	 */
+	@Test
+	public void testIsOprCprValidPositive00() {
+		String oprCpr = "0208931111";
+
+		boolean actual = oprValidator.isOprCprValid(oprCpr);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprCpr + " is supposed to be valid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprCprValidPositive01() {
+		String oprCpr = "0000000000";
+
+		boolean actual = oprValidator.isOprCprValid(oprCpr);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprCpr + " is supposed to be valid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprCprValidPositive02() {
+		String oprCpr = "9999999999";
+
+		boolean actual = oprValidator.isOprCprValid(oprCpr);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprCpr + " is supposed to be valid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprCprValidNegative00() {
+		String oprCpr = "";
+
+		boolean actual = oprValidator.isOprCprValid(oprCpr);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprCpr + " is supposed to be invalid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprCprValidNegative01() {
+		String oprCpr = "asd";
+
+		boolean actual = oprValidator.isOprCprValid(oprCpr);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprCpr + " is supposed to be invalid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprCprValidNegative02() {
+		String oprCpr = "11111111111111111111111111111111111";
+
+		boolean actual = oprValidator.isOprCprValid(oprCpr);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprCpr + " is supposed to be invalid.", expected, actual);
+	}
+	
+	/*
+	 * isOprPasswordValid test.
+	 */
+	@Test
+	public void testIsOprPasswordValidPositive00() {
+		String oprPassword = "11111";
+
+		boolean actual = oprValidator.isOprPasswordValid(oprPassword);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprPassword + " is supposed to be valid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprPasswordValidPositive01() {
+		String oprPassword = "111111111111111111111111111111";
+
+		boolean actual = oprValidator.isOprPasswordValid(oprPassword);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprPassword + " is supposed to be valid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprPasswordValidPositive02() {
+		String oprPassword = "111ojsfdgpojdfg11";
+
+		boolean actual = oprValidator.isOprPasswordValid(oprPassword);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprPassword + " is supposed to be valid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprPasswordValidNegative00() {
+		String oprPassword = "";
+
+		boolean actual = oprValidator.isOprPasswordValid(oprPassword);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprPassword + " is supposed to be invalid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprPasswordValidNegative01() {
+		String oprPassword = "jjjjjjjjjjttttttttttggggggggggqqqqqqqqqq";
+
+		boolean actual = oprValidator.isOprPasswordValid(oprPassword);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprPassword + " is supposed to be invalid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprPasswordValidNegative02() {
+		String oprPassword = "keklordpelbalmightymaster007-kekplebimba1337";
+
+		boolean actual = oprValidator.isOprPasswordValid(oprPassword);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprPassword + " is supposed to be invalid.", expected, actual);
+	}
+
+	/*
+	 * isOprStatusValid test.
+	 */
+	@Test
+	public void testIsOprStatusValidPositive00() {
+		int oprStatus = 0;
+
+		boolean actual = oprValidator.isOprStatusValid(oprStatus);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprStatus + " is supposed to be valid.", expected, actual);
+	}
+
+	@Test
+	public void testIsOprStatusValidPositive01() {
+		int oprStatus = 1;
+
+		boolean actual = oprValidator.isOprStatusValid(oprStatus);
+		boolean expected = true;
+
+		assertEquals("Failed: " + oprStatus + " is supposed to be valid.", expected, actual);
+	}
+	
+	@Test
+	public void testIsOprStatusValidFalse00() {
+		int oprStatus = 3;
+
+		boolean actual = oprValidator.isOprStatusValid(oprStatus);
+		boolean expected = false;
+
+		assertEquals("Failed: " + oprStatus + " is supposed to be valid.", expected, actual);
+	}
+
+	
+	
 }
