1aac92bdf1666bfad608c37e44bcb9c95df23954
InAbsencia <absentia@protonmail.ch>
2017-06-09 12:50:12 +0200

    cry

--- 11_CDIO_FINAL/src/final_cdio_11/java/weight/sim/socket/ISocketController.java
+++
@@ -1,9 +0,0 @@
-package final_cdio_11.java.weight.sim.socket;
-
-public interface ISocketController extends Runnable {
-	public final static int PORT = 8000;
-	void registerObserver(ISocketObserver observer);
-	void unRegisterObserver(ISocketObserver observer);
-	void notifyObservers(SocketInMessage message);
-	void sendMessage(SocketOutMessage message);
-}
--- 11_CDIO_FINAL/src/final_cdio_11/java/weight/sim/socket/ISocketObserver.java
+++
@@ -1,5 +0,0 @@
-package final_cdio_11.java.weight.sim.socket;
-
-public interface ISocketObserver {
-	void notify(SocketInMessage message);
-}
